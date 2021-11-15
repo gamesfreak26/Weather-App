a7ca42e820bbf5cc82cd768feabdea84f4d977db
Tharanga Kasthuriarachchi <tkasthuriarachchi@pccwglobal.com>
2021-11-15 12:47:54 +1000

    get weather data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  if (!process.env.REACT_APP_API_KEY) {
+    console.log(`Cannot find REACT_APP_API_KEY key: ${process.env.REACT_APP_API_KEY}`);
+  }
+  
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+
+    const apiKey = process.env.REACT_APP_API_KEY;
+    
+    const getFetchUrl = () => {
+      return 'https://api.openweathermap.org/data/2.5/forecast?q=Brisbane,AU&appid=' + apiKey +'&units=metric'
+    }
+    
+    async function fetchData() {
+      const result = await axios(getFetchUrl());
+      console.log(result)
+      setData(result.data);
+    }
+    
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
     </div>
   );
 }
